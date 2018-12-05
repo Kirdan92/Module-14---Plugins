74a2c7fe62587b0365a63822553b8ed01b39bba5
Pawel Butra <pawel.butra@onet.pl>
2018-12-05 17:21:03 +0100

    Add basic carousel logic and content

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 'use strict';
 (function(){ 
+var elem = document.querySelector('.carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  wrapAround: true,
+  freeScroll: true,
+  hash: true
+  //autoPlay: 5000
+});
 
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.carousel', {
+  // options
+});
   
 })(); 
