e029d9f51534c9231336c147be3ab7732ebd498f
Pawel Butra <pawel.butra@onet.pl>
2018-12-07 12:45:55 +0100

    Add Container styles and Add google map

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 'use strict';
 (function(){ 
 
-//Slides template
+// Gooogle Maps init	
+window.initMap = function(){
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
+
+
+//Mustache Slides template
 	var templateSlide = document.getElementById('template-slide-item').innerHTML;
 	
 
